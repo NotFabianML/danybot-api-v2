d65324ee46e744b91f5b587d79088ee8e6634650
NotFabianML <80232944+NotFabianML@users.noreply.github.com>
2025-04-24 02:20:01 -0600

    fix: Update /download route from GET to POST

--- routes/pickandplace/create_gcode_routes.py
+++ routes/pickandplace/create_gcode_routes.py
@@ -10,11 +10,29 @@ gcode = {"gcode": "test - ok"}
 def get_gcode():
     return jsonify(gcode), 200
 
-@gcode_bp.route('/download', methods=['GET'])
+# @gcode_bp.route('/download', methods=['GET'])
+# def send_dynamic_gcode():
+#     data = request.get_json()
+#     print("Data: ", data)
+#     file_name = data.get('filename')  # e.g. 'routine_001.gcode
+
+
+#     # Ruta para apuntar al directorio de archivos GCODE
+#     folder = "core/modules/pick_and_place/gcode_files"
+#     base_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))  # Ruta base del proyecto
+#     file_path = os.path.join(base_path, folder, file_name)
+#     print("File Path: ", file_path)
+
+#     if not os.path.exists(file_path):
+#         return jsonify({"error": "File not Found", "filename": file_name, "filepath":file_path}), 404
+
+#     return send_file(file_path, as_attachment=True)
+# 
+@gcode_bp.route('/download', methods=['POST'])
 def send_dynamic_gcode():
-    data = request.get_json()
-    print("Data: ", data)
-    file_name = data.get('filename')  # e.g. 'routine_001.gcode
+    payload = request.get_json()
+    print("Data: ", payload)
+    file_name = payload.get('filename')
 
 
     # Ruta para apuntar al directorio de archivos GCODE
